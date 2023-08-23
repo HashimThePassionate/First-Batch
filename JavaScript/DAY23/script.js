7cf4dbadd60b6b803216c4fc2aa044b6a6ddcb43
hashimthepassionate <hashiimtahir@gmail.com>
2023-08-23 20:47:52 +0500

    pushed

---
+++ JavaScript/DAY23/script.js
@@ -0,0 +1,97 @@
+
+//Loops
+//Increment / Decrement
+// var a = 1;
+// console.log(a);
+// console.log(a++);
+// console.log(a);
+// // Postfix increment / Postfix Decrement a++ / a--
+// // Prefix increment / Prefix Decrement ++a / --a
+// var b =1;
+// console.log(b);
+// console.log(++b);
+// if(b===2)
+// {
+//     console.log("True");
+// }
+// for(initialn;increment/decrement;conditio)
+// {
+        // console.log("Pakistan");
+// }
+// var x  = 2;
+// console.log(x);
+// x = 3;
+// console.log(x);
+// JavaScript loops are control structures that allow you to repeatedly execute a block of code as long as a certain condition is true
+//For Loop
+// The for loop is commonly used when you know how many times you want to repeat a block of code.
+// for(initilization;condition;increment/decrement)
+// {
+        // Statements
+// }
+// var myArray = [];
+// var limit = parseInt(prompt("Enter Limit to repeat For loop"));
+// for (let k = 0; k < limit;k++) {
+//     var temp = parseInt(prompt("Enter Number"));
+//     myArray.push(temp);
+//     console.log(`Array Index ${k+1} = ${myArray[k]}`);
+// }
+//Table 
+// for(var i = 10;i>0;i--)
+// {
+//     console.log("Welcome to JavaScript Programming");
+// }
+// var tableNo = parseInt(prompt("Please Enter Table No"));
+// var tableLimit = parseInt(prompt("Please Enter table limit "));
+// for (let j = 1; j <= tableLimit; j++) {
+//   console.log(`${tableNo} * ${j} = ${tableNo*j}`);
+// }
+
+// While Loop
+// The while loop repeats a block of code as long as a specified condition is true
+// var count = 1;
+// while(count<=5)1<=5
+// {
+//     console.log(count);
+//     count++;
+// }
+// var rn = Math.floor(Math.random()*10);
+// var nooftries = 0;
+// var playGuess;
+// while(playGuess!==rn)
+// {
+//     playGuess = parseInt(prompt("Guess The Number :"));
+//     nooftries++;
+//     if(playGuess<rn)
+//     {
+//         console.log("You are to Low!");
+//     }
+//     else
+//     {
+//         console.log("You are two High!");
+//     }
+// }
+// console.log(`Congratulation You guessed the number ${nooftries} tries`);
+// Do While Loop
+// var i = 1;
+// do {
+//     console.log("Hello Do While");
+//     i++;
+// }while(i>5)
+// var pass = "hashimtahir";
+// var userpass;
+// var  attempts = 0;
+// do {
+//     userpass = prompt("Please Enter your password");
+//     attempts++;
+//     if(userpass!==pass)
+//     {
+//         console.log("Incorrect Password Please try again!");
+//     }
+// } while (userpass!==pass);
+// alert("Authentication successful it took you "+attempts+" attempts");
+// For in Loop
+// terating over the enumerable properties of an object, such as the properties of an array or an object.
+
+
+// For of Loop
