8ddfb04751315997710e187185b01d7cb6fda755
hashimthepassionate <hashiimtahir@gmail.com>
2023-08-25 19:28:51 +0500

    pushed

---
+++ JavaScript/Day25/script.js
@@ -0,0 +1,36 @@
+// var ul =  document.getElementById('ul');
+// ul.style.height="300px";
+// ul.style.width="100%";
+// ul.style.border="2px solid salmon";
+// ul.style.display="flex";
+// ul.style.padding="20px 30px";
+// ul.style.flexDirection="column";
+// ul.style.listStyleType="square";
+// var createLiNode = document.createElement('li');
+// var createLiText = document.createTextNode('java');
+// createLiNode.appendChild(createLiText);
+// ul.appendChild(createLiNode);
+// var refNode = document.getElementById('item33');
+// // var newNode = document.createElement('li');
+// // var newNodeText = document.createTextNode("machine");
+// // newNode.appendChild(newNodeText);
+// var a  = ul.insertBefore(createLiNode,ul.children[2]);
+// var newchildNode =  document.createElement('li');
+// ul.insertBefore(newchildNode,refNode);
+// var newChildText = document.createTextNode('Jquery');
+// newchildNode.appendChild(newChildText);
+// newchildNode.remove();
+// var item4 =  document.getElementById('item44');
+// // item4.
+// item4.remove();
+
+// var button = document.getElementById('button');
+// var con = document.getElementById('con');
+// function trigger() {
+//     // con.style.textAlign="center";
+//     con.style.border="2px solid darksalmon";
+//     con.style.backgroundColor="gray";
+//     con.style.color="black";    
+// }
+
+// button.addEventListener('click',trigger);
